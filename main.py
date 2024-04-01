cd96ad42c6176f55072bea6cb4dde6ebec9d726d
Roman Voropaev <voropaev.roma@gmail.com>
2024-04-01 15:57:09 +0300

    Simpler way

--- main.py
+++ main.py
@@ -16,15 +16,12 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/screen")
+@app.route("/screen.png")
 def screen():
     num = 0
     fname = next(sct.save(mon=num, output="/tmp/mon-{mon}.png"))
     with open(fname, "rb") as scr:
-        return (
-            "<img src='data:image/png;base64,"
-            f"{b64encode(scr.read()).decode()}' alt='Desktop TV'/>"
-        )
+        return send_file(BytesIO(scr.read()), mimetype="image/png")
 
 
 if __name__ == "__main__":
