c218c0bc8f877f1b3971af0e832427591e0b9098
Roman Voropaev <voropaev.roma@gmail.com>
2024-05-04 19:28:23 +0300

    No flickering with png updates via multipart

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from base64 import b64encode
 from io import BytesIO
 
 import ngrok
+import numpy as np
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, send_file
-from mss import mss
+from flask import Flask, Response, render_template, request
+from mss import mss, tools
+
+from jpeg import to_jpeg
 
 load_dotenv()
 app = Flask(__name__)
@@ -18,10 +21,18 @@ def home():
 
 @app.route("/screen")
 def screen():
-    num = 0
-    fname = next(sct.save(mon=num, output="/tmp/mon-{mon}.png"))
-    with open(fname, "rb") as scr:
-        return send_file(BytesIO(scr.read()), mimetype="image/png")
+    def frames():
+        mon = sct.monitors[0]
+        while True:
+            shot = sct.grab(mon)
+            img_bytes = tools.to_png(shot.rgb, shot.size)
+            # img_bytes = to_jpeg(np.array(shot)[:, :, :3])
+            yield (
+                b"--frame\r\nContent-Type: image/png\r\nContent-Size:%d\r\n\r\n%s\r\n"
+                % (len(img_bytes), img_bytes)
+            )
+
+    return Response(frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
